0b832912cf7f9b302902b8d54e4b12e0f1b723de
Pabl0Romano <romae047@gmail.com>
2022-11-02 08:58:21 -0300

    trabajo

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.listen(3000,()=>
+app.listen(4000,()=>
     console.log("Server UP")
 );
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+});
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
 });
